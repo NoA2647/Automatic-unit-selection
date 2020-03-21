1c5c722298f98fc9f8548e074f1d23228e6f29cc
Amir <62354732+NoA2647@users.noreply.github.com>
2020-03-21 11:33:34 +0430

    add command & some edit

--- portal.py
+++ portal.py
@@ -1,17 +1,17 @@
+#Hello , this program get study from portal of aut automaticly
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-driver = webdriver.Firefox()
-driver.get("https://portal.aut.ac.ir/aportal/")
-# while()
+driver = webdriver.Firefox()#open firefox
+driver.get("https://portal.aut.ac.ir/aportal/")#go to this url
 id_box = driver.find_element_by_name('username')
-id_box.send_keys('9713042')
+id_box.send_keys('*******')#Enter the student number
 id_box = driver.find_element_by_id('password')
-id_box.send_keys('amir2647')
+id_box.send_keys('*******')#Enter the password
 id_box = driver.find_element_by_id('passline')
 id_box.click()
-time.sleep(5)
+time.sleep(5)#waiting to enter pass-picture  
 id_box = driver.find_element_by_class_name('logbutton')
 id_box.click()
 driver.switch_to_default_content()
@@ -22,15 +22,15 @@ id_box.click()
 driver.switch_to_default_content()
 frame = driver.find_element_by_name("main")
 driver.switch_to_frame(frame)
-time.sleep(0.05)
+time.sleep(0.05)#waiting to load
 id_box = driver.find_element_by_name('st_reg_courseid')
-id_box.send_keys('1218393')
+id_box.send_keys('*******')#Enter code of study
 id_box = driver.find_element_by_name('st_reg_groupno')
-id_box.send_keys('1')
+id_box.send_keys('*******')#Enter group of study
 id_box = driver.find_element_by_name('addpassline')
 id_box.click()
-time.sleep(2)
+time.sleep(2)#waiting for enter pass-picture
 id_box = driver.find_element_by_name('st_course_add')
 id_box.click()
-time.sleep(0.05)
-driver.save_screenshot("/home/noa/Desktop/portal.png")
+time.sleep(0.05)#waiting to load
+driver.save_screenshot("*******")#get screenshot & saved in <dir>
