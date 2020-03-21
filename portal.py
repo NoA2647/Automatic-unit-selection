0d6fc54961c773bd03736ba67b61bd5d965bc913
amir <amir.h.joghataei003@gmail.com>
2020-03-21 11:38:57 +0430

    add space between code and command

--- portal.py
+++ portal.py
@@ -3,15 +3,15 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-driver = webdriver.Firefox()#open firefox
-driver.get("https://portal.aut.ac.ir/aportal/")#go to this url
+driver = webdriver.Firefox() #open firefox
+driver.get("https://portal.aut.ac.ir/aportal/") #go to this url
 id_box = driver.find_element_by_name('username')
-id_box.send_keys('*******')#Enter the student number
+id_box.send_keys('*******') #Enter the student number
 id_box = driver.find_element_by_id('password')
-id_box.send_keys('*******')#Enter the password
+id_box.send_keys('*******') #Enter the password
 id_box = driver.find_element_by_id('passline')
 id_box.click()
-time.sleep(5)#waiting to enter pass-picture  
+time.sleep(5) #waiting to enter pass-picture  
 id_box = driver.find_element_by_class_name('logbutton')
 id_box.click()
 driver.switch_to_default_content()
@@ -22,15 +22,15 @@ id_box.click()
 driver.switch_to_default_content()
 frame = driver.find_element_by_name("main")
 driver.switch_to_frame(frame)
-time.sleep(0.05)#waiting to load
+time.sleep(0.05) #waiting to load
 id_box = driver.find_element_by_name('st_reg_courseid')
-id_box.send_keys('*******')#Enter code of study
+id_box.send_keys('*******') #Enter code of study
 id_box = driver.find_element_by_name('st_reg_groupno')
-id_box.send_keys('*******')#Enter group of study
+id_box.send_keys('*******') #Enter group of study
 id_box = driver.find_element_by_name('addpassline')
 id_box.click()
-time.sleep(2)#waiting for enter pass-picture
+time.sleep(2) #waiting for enter pass-picture
 id_box = driver.find_element_by_name('st_course_add')
 id_box.click()
-time.sleep(0.05)#waiting to load
-driver.save_screenshot("*******")#get screenshot & saved in <dir>
+time.sleep(0.05) #waiting to load
+driver.save_screenshot("*******") #get screenshot & saved in <dir>
