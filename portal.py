dd66eef6b72916e4865a79b1ff7ce6907d19e83e
NoA2647 <amir.h.joghataei003@gmail.com>
2020-09-01 10:04:26 +0430

    add variable of user name and password and add for and list

--- portal.py
+++ portal.py
@@ -3,13 +3,17 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+userName = "" # enter user name
+password = "" # enter password
+listens = [{"listenId":"","groupId":""}] #enter listen id and group id like this ,its dictionery
 driver = webdriver.Firefox() #open firefox
 driver.get("https://portal.aut.ac.ir/aportal/") #go to this url
 id_box = driver.find_element_by_name('username')
-id_box.send_keys('*******') #Enter the student number
+id_box.send_keys(userName) #Enter the student number
 id_box = driver.find_element_by_id('password')
-id_box.send_keys('*******') #Enter the password
+id_box.send_keys(password) #Enter the password
 id_box = driver.find_element_by_id('passline')
+#print(id_box)
 id_box.click()
 time.sleep(5) #waiting to enter pass-picture  
 id_box = driver.find_element_by_class_name('logbutton')
@@ -17,20 +21,23 @@ id_box.click()
 driver.switch_to_default_content()
 frame = driver.find_element_by_name("side")
 driver.switch_to_frame(frame)
-id_box = driver.find_element_by_xpath("//div[@id='Bar_panel0_b1']")
+id_box = driver.find_element_by_xpath("//div[@id='Bar_panel1_c']")
+id_box.click()
+id_box = driver.find_element_by_xpath("//div[@id='Bar_panel1_b0']")
 id_box.click()
 driver.switch_to_default_content()
 frame = driver.find_element_by_name("main")
 driver.switch_to_frame(frame)
-time.sleep(0.05) #waiting to load
-id_box = driver.find_element_by_name('st_reg_courseid')
-id_box.send_keys('*******') #Enter code of study
-id_box = driver.find_element_by_name('st_reg_groupno')
-id_box.send_keys('*******') #Enter group of study
-id_box = driver.find_element_by_name('addpassline')
-id_box.click()
-time.sleep(2) #waiting for enter pass-picture
-id_box = driver.find_element_by_name('st_course_add')
-id_box.click()
-time.sleep(0.05) #waiting to load
-driver.save_screenshot("*******") #get screenshot & saved in <dir>
+time.sleep(1) #waiting to load
+for listen in listens:
+    id_box = driver.find_element_by_name('st_reg_courseid')
+    id_box.send_keys(listen["listenId"]) #Enter code of study
+    id_box = driver.find_element_by_name('st_reg_groupno')
+    id_box.send_keys(listen["groupId"]) #Enter group of study
+    id_box = driver.find_element_by_name('addpassline')
+    id_box.click()
+    time.sleep(2) #waiting for enter pass-picture
+    id_box = driver.find_element_by_name('st_course_add')
+    id_box.click()
+    time.sleep(0.5) #waiting to load
+driver.save_screenshot("./screenShot.png") #get screenshot & saved in <dir>
